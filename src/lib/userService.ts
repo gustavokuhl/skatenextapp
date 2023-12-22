b17e18669ca9c78e03b28e98338cd0f14ba2b56b
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-12-22 20:37:11 -0300

    fix: change function to public

--- src/lib/userService.ts
+++ src/lib/userService.ts
@@ -1,12 +1,11 @@
-"use server"
-
 import HiveClient from "@/lib/hiveclient"
 import User from "@/lib/user"
 
 const hiveClient = HiveClient()
 
 export default class UserService {
-  async getByName(username: string): Promise<User> {
+  public async getByName(username: string): Promise<User> {
+    "use server"
     try {
       const data = await hiveClient.database.getAccounts([username])
       if (data.length === 0) throw new Error("User not found")
