e26ce49dbb6882bb4258aaa4221295e61653a63f
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-12-24 08:14:37 -0300

    refactor: update feed to new post interface

--- src/components/FeedPage.tsx
+++ src/components/FeedPage.tsx
@@ -1,28 +1,26 @@
 import HiveClient from "@/lib/hiveclient"
+import PostModel, { PostProps } from "@/lib/models/post"
 import { VStack } from "@chakra-ui/react"
 import Post from "./Post"
 const SKATEHIVE_TAG = "hive-173115"
 
 const hiveClient = HiveClient()
 
-async function getData(threshold: number = 0) {
-  const discussions = await hiveClient.database.getDiscussions("created", {
+async function getData(threshold: number = 0): Promise<PostProps[]> {
+  const response = await hiveClient.database.getDiscussions("created", {
     tag: SKATEHIVE_TAG,
     limit: threshold + 20,
   })
-
-  if (discussions.length === 0) {
-    throw new Error("Failed to fetch discussions")
-  }
-
-  return discussions
+  if (Array.isArray(response) && response.length > 0)
+    return response.map((post) => PostModel.simplifyFromDiscussion(post))
+  return [{} as PostProps]
 }
 
 export default async function FeedPage() {
   const data = await getData()
   return (
     <VStack align="stretch" spacing={4} p={2}>
-      {data && data.map((post, i) => <Post key={i} post={post} />)}
+      {data && data.map((post, i) => <Post key={i} postData={post} />)}
     </VStack>
   )
 }
